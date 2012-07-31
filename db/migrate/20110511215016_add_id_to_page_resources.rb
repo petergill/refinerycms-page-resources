9f4d2ab03d8d693f184b4c66b500cf132825b177
Chris Lusted <kipl20@hotmail.com>
2012-07-31 15:25:10 +0100

    add id to page resources
    Signed-off-by: Chris Lusted <kipl20@hotmail.com>

--- db/migrate/20110511215016_add_id_to_page_resources.rb
+++ db/migrate/20110511215016_add_id_to_page_resources.rb
@@ -1,4 +1,4 @@
-class TranslatePageImageCaptions < ActiveRecord::Migration
+class AddIdToPageResources < ActiveRecord::Migration
   def change
     add_column Refinery::ResourcePage.table_name, :id, :primary_key
   end
