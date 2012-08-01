c7f2f67905a3a3b12623c3518a1e9788d462b561
Chris Lusted <kipl20@hotmail.com>
2012-08-01 14:59:06 +0100

    Wrong class name!
    Signed-off-by: Chris Lusted <kipl20@hotmail.com>

--- db/migrate/20110527052435_change_page_resource_to_polymorphic.rb
+++ db/migrate/20110527052435_change_page_resource_to_polymorphic.rb
@@ -1,5 +1,5 @@
 class ChangePageResourceToPolymorphic < ActiveRecord::Migration
   def change
-    add_column Refinery::PageResource.table_name, :page_type, :string, :default => "page"
+    add_column Refinery::ResourcePage.table_name, :page_type, :string, :default => "page"
   end
 end
