ca1f854627b3b059ec18b629e293ec0c4de4761e
Chris Lusted <kipl20@hotmail.com>
2012-08-01 14:51:17 +0100

    change name of migration to stop conflict with page-images
    Signed-off-by: Chris Lusted <kipl20@hotmail.com>

--- db/migrate/20110527052435_change_page_resource_to_polymorphic.rb
+++ db/migrate/20110527052435_change_page_resource_to_polymorphic.rb
@@ -1,4 +1,4 @@
-class ChangePageToPolymorphic < ActiveRecord::Migration
+class ChangePageResourceToPolymorphic < ActiveRecord::Migration
   def change
     add_column Refinery::PageResource.table_name, :page_type, :string, :default => "page"
   end
